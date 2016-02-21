46569ab46053a75527ea77e1d0dcdc0c00ec205d
Todd Kenna <i.toddicus@gmail.com>
2016-02-21 13:26:59 -0600

    scoring and JS completed

--- js/script.js
+++ js/script.js
@@ -3,21 +3,14 @@ $(function(){
 	var turn = 0
 	var scoreArray = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0};
 	var playerScore = {0:0,1:0};
-			
+		{
 			$('.field2').on('click', function(){
 			var id = this.id;
-			//console.log(this.id)
-			//console.log(turn)
-			//alert("You have pushed button " + id);
 					if(turn == 0) {
-						//alert("You have pushed button " + id);
 						$(this).removeClass('hid')
 						scoreArray[id] = 1;
 						turn = turn + 1
-						tracker = tracker + 1
-						//console.log(turn)
-						//console.log(scoreArray);
-						
+						tracker = tracker + 1						
 					}
 					else if(turn ==1) {
 						$(this).find('img').removeAttr('src', 'file:///home/toddzio/code/wdi/myWebsites/Tic-Tac-Toes/assets/toe1crop.jpg')
@@ -26,84 +19,93 @@ $(function(){
 						scoreArray[id] = 10;
 						turn = turn -1
 						tracker = tracker + 1
-						//console.log(turn)
-					}
-				score()				
+					};
+			Score()			
 			});
+};		
 	$('#reload').click(function() {
 			location.reload();
 	});		
 	$('#reset').click(function() {
 			scoreArray = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
-			//playerScore = {0:0,1:0}
+			tracker = 0
+			turn = 0
+			$('.field2').addClass('hid')
+			$('.field2').find('img').removeAttr('src', 'file:///home/toddzio/code/wdi/myWebsites/Tic-Tac-Toes/assets/toe2crop.jpg')
+			$('.field2').find('img').attr('src', 'file:///home/toddzio/code/wdi/myWebsites/Tic-Tac-Toes/assets/toe1crop.jpg')
+	});
+	function reset(){
+			scoreArray = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
 			tracker = 0
 			turn = 0
 			$('.field2').addClass('hid');
-			//console.log(scoreArray);
-			//console.log(playerScore);
-	});
-	//function reset(){
-			//scoreArray = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
-			//playerScore = {0:0,1:0}
-			//tracker = 0
-			//turn = 0
-			//$('.field2').addClass('hid');
-	});
-	function score() {
-		console.log(scoreArray)
+	}
+function calcScore() {
+		if (turn == 1) {
+			score = (playerScore[0] + 1)
+			playerScore[0] = score
+			alert("Player 1 has won!")
+			$('#player1').html(score)
+		}
+		else if (turn == 0) {
+			score = (playerScore[1] + 1)
+			playerScore[1] = score
+			alert("Player 2 has won!")
+			$('#player2').html(score)
+		}
+	};
+function Score() {
+
 		if (scoreArray[1] + scoreArray[2] + scoreArray[3] == 3) {
-			alert("Why did this break?");
+			calcScore();		
 		}
 		else if (scoreArray[4] +scoreArray[5] + scoreArray[6] ==3) {
-			calcScore()
+			calcScore();
 		}
 		else if (scoreArray[7] +scoreArray[8] + scoreArray[9] ==3) {
-			calcScore()
+			calcScore();
 		}
 		else if (scoreArray[1] +scoreArray[4] + scoreArray[7] ==3) {
-			calcScore()
+			calcScore();
 		}
 		else if (scoreArray[2] +scoreArray[5] + scoreArray[8] ==3) {
-			calcScore()
+			calcScore();
 		}
 		else if (scoreArray[3] +scoreArray[6] + scoreArray[9] ==3) {
-			calcScore()
+			calcScore();
 		}
 		else if (scoreArray[1] +scoreArray[5] + scoreArray[9] ==3) {
-			calcScore()
+			calcScore();
 		}
 		else if (scoreArray[3] +scoreArray[5] + scoreArray[7] ==3) {
-			calcScore()
+			calcScore();
 		}
-		else if (scoreArray[1] + scoreArray[2] + scoreArray[3] == 30) {
-			calcScore()
+		else if (scoreArray[1] +scoreArray[2] +scoreArray[3] == 30) {
+			calcScore();
 		}
-		else if (scoreArray[4] +scoreArray[5] + scoreArray[6] ==30) {
-			calcScore()
+		else if (scoreArray[4] +scoreArray[5] +scoreArray[6] ==30) {
+			calcScore();
 		}
-		else if (scoreArray[7] +scoreArray[8] + scoreArray[9] ==30) {
-			calcScore()
+		else if (scoreArray[7] +scoreArray[8] +scoreArray[9] ==30) {
+			calcScore();
 		}
-		else if (scoreArray[1] +scoreArray[4] + scoreArray[7] ==30) {
-			calcScore()
+		else if (scoreArray[1] +scoreArray[4] +scoreArray[7] ==30) {
+			calcScore();
 		}
-		else if (scoreArray[2] +scoreArray[5] + scoreArray[8] ==30) {
-			calcScore()
+		else if (scoreArray[2] +scoreArray[5] +scoreArray[8] ==30) {
+			calcScore();
 		}
-		else if (scoreArray[3] +scoreArray[6] + scoreArray[9] ==30) {
-			calcScore()
+		else if (scoreArray[3] +scoreArray[6] +scoreArray[9] ==30) {
+			calcScore();
 		}
-		else if (scoreArray[1] +scoreArray[5] + scoreArray[9] ==30) {
-			calcScore()
+		else if (scoreArray[1] +scoreArray[5] +scoreArray[9] ==30) {
+			calcScore();
 		}
-		else if (scoreArray[3] +scoreArray[5] + scoreArray[7] ==30) {
-			calcScore()
+		else if (scoreArray[3] +scoreArray[5] +scoreArray[7] ==30) {
+			calcScore();
 		}
 		else if (tracker > 7){
 			alert("TIE!")
 		};
 	}
-	//function board(){
-		//var board = 0
-		//$('#checkingBalance').html('$' + total);
-//});
+})
